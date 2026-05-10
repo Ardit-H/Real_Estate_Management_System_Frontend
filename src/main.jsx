c5847c0c4ca35ffd66a3024deadd8979fced5ef6
Ardit-H <ardit.hyseni5@student.uni-pr.edu>
2026-05-10 13:32:43 +0200

    konfigurova QueryClientProvider me staleTime 5min ne main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./App";
+import AuthProvider from "./context/AuthProvider";
 
-import App from "./App.jsx";
-import AuthProvider from "./context/AuthProvider.jsx";
-import "./index.css";
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000,
+      gcTime:    10 * 60 * 1000,
+      retry: 1,
+    },
+  },
+});
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <AuthProvider>
-                <App />
-            </AuthProvider>
-        </BrowserRouter>
-    </React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
+  </StrictMode>
 );
